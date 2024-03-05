95a8dd861e6ab4d1457ca4e7b6f0f050e2151433
balkanarchist <balkanarchist@yandex.com>
2024-03-05 09:32:04 -0700

    added code to identity-operators-task2

---
+++ identity-operators-task2.py
@@ -0,0 +1,14 @@
+# Check if the two lists are identical in terms of their content, regardless of order.
+
+# to compare two lists, use if (A) is (B): / else:
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if (submitted) is (attended):
+    print("Huzzah! The two lists are identical in terms of their content, regardless of order.")
+
+else:
+    print("Ruh roh. The two lists are not identical.")
+
+# There were no instructions to print - they're just included to check if the code works as expected.
