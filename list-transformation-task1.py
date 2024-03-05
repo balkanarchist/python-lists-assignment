f8cf4788965e6f9c006be42fc43d3c3f4b0150ca
balkanarchist <balkanarchist@yandex.com>
2024-03-05 08:28:42 -0700

    added code to list-transformation-task1

---
+++ list-transformation-task1.py
@@ -0,0 +1,13 @@
+'''
+You've been given a list of numerical grades from a class exam.
+You need to process and analyze these grades.
+
+Task 1: Given the list of grades: grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+Sort the grades in descending order and display the sorted list.
+'''
+# Create the variable, sort it (or sort then reverse) ), then print it.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort(reverse=True)
+print(grades)
