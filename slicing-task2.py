7d2718af27a81f16024773c22a78c61b5555a36d
balkanarchist <balkanarchist@yandex.com>
2024-03-05 10:51:21 -0700

    added code to slicing-task2

---
+++ slicing-task2.py
@@ -0,0 +1,10 @@
+# Extract all the temperatures above 100.
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+
+# Maybe just use simple slicing
+
+print(temperatures[24:])
+
+# I imagine there's an easier way to do this (when you have a large set of ints) instead
+# of just counting which position the starting point would be at, but this method works too.
