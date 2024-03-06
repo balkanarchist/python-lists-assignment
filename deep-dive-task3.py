906364a94879fb00fe35f3f33f592131bbe443c5
balkanarchist <balkanarchist@yandex.com>
2024-03-06 00:11:24 -0700

    revised code and comments for deep-dive-task3

--- deep-dive-task3.py
+++ deep-dive-task3.py
@@ -5,15 +5,15 @@ students = ["John", "Doe", "Jane", "Smith"]
 grades = [85, 90, 78, 88]
 activities = ["Football", "Music", "Art", "Dance"]
 '''
-# Only change from end of Task 2 should be creation of #students_approved variable with remaining students
+# Only change from end of Task 2 should be creation of #students_approved variable
+# with remaining students
 
 students = ["John", "Doe", "Jane", "Smith"]
 grades = [85, 90, 78, 88]
 activities = ["Football", "Music", "Art", "Dance"]
 
-profile1 = [students[0], grades[0], activities[0]]
-profile2 = [students[1], grades[1], activities[1]]
-profile4 = [students[3], grades[3], activities[3]]
+# Per directions, we only need the name and not the grades and activities this time
 
-students_approved = (profile1, profile2, profile4)
+students.pop(2)
+students_approved = students #Jane should now be removed
 print(students_approved)
