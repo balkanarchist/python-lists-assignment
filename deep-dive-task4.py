b99ee28f175bb266252d4089566479819a6d3e71
balkanarchist <balkanarchist@yandex.com>
2024-03-06 00:19:20 -0700

    revised code for deep-dive-task4

--- deep-dive-task4.py
+++ deep-dive-task4.py
@@ -1,13 +1,9 @@
 # Print the list students_approved
-# Should be no change from task3 except to print the result
 
 students = ["John", "Doe", "Jane", "Smith"]
 grades = [85, 90, 78, 88]
 activities = ["Football", "Music", "Art", "Dance"]
 
-profile1 = [students[0], grades[0], activities[0]]
-profile2 = [students[1], grades[1], activities[1]]
-profile4 = [students[3], grades[3], activities[3]]
-
-students_approved = (profile1, profile2, profile4)
+students.pop(2)
+students_approved = students #Jane should now be removed
 print(students_approved)
