e96c0b4652f9c3e32d682666342d6ebdeb84a5fc
balkanarchist <balkanarchist@yandex.com>
2024-03-06 00:15:50 -0700

    revised code and comments for deep-dive-task2

--- deep-dive-task2.py
+++ deep-dive-task2.py
@@ -12,16 +12,10 @@ students = ["John", "Doe", "Jane", "Smith"]
 grades = [85, 90, 78, 88]
 activities = ["Football", "Music", "Art", "Dance"]
 
-profile1 = [students[0], grades[0], activities[0]]
-profile2 = [students[1], grades[1], activities[1]]
-profile3 = [students[2], grades[2], activities[2]] # the grade here is below 80
-profile4 = [students[3], grades[3], activities[3]]
+profile: dict = {0: [students[0], grades[0], activities[0]], 1: [students[1], grades[1], activities[1]], 2: [students[2], grades[2], activities[2]], 3: [students[3], grades[3], activities[3]]}
+print(profile) #for verification that variables were combined correctly
+print() #for readability
 
-del profile3
-remaining_students = (profile1, profile2, profile4)
-
-print(remaining_students) #for verification
-
-# I'm sure we're meant to write a code that would remove "profile3" automatically
-# but based on information we've been given in the course so far, I don't know how
-# to do that, except manually.
+# Add variable to print only Jane's profile
+grade_under_80 = profile.pop(2)
+print(grade_under_80)
