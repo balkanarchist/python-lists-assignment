94c6964dccd3ae00f5c15d1b8202bb7fc53ce96d
balkanarchist <balkanarchist@yandex.com>
2024-03-05 21:33:40 -0700

    added code to deep-dive-task3

---
+++ deep-dive-task3.py
@@ -0,0 +1,18 @@
+'''
+For the remaining students, add students name in a new list named students_approved
+
+students = ["John", "Doe", "Jane", "Smith"]
+grades = [85, 90, 78, 88]
+activities = ["Football", "Music", "Art", "Dance"]
+'''
+
+students = ["John", "Doe", "Jane", "Smith"]
+grades = [85, 90, 78, 88]
+activities = ["Football", "Music", "Art", "Dance"]
+
+profile1 = [students[0], grades[0], activities[0]]
+profile2 = [students[1], grades[1], activities[1]]
+profile4 = [students[3], grades[3], activities[3]]
+
+remaining_students = (profile1, profile2, profile4)
+print(remaining_students)
