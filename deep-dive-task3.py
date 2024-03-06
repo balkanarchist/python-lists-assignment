32f32e16d17a4b3ce32f76c77aae02aed249e485
balkanarchist <balkanarchist@yandex.com>
2024-03-05 21:37:27 -0700

    added code and comments to deep-dive-task3

--- deep-dive-task3.py
+++ deep-dive-task3.py
@@ -5,6 +5,7 @@ students = ["John", "Doe", "Jane", "Smith"]
 grades = [85, 90, 78, 88]
 activities = ["Football", "Music", "Art", "Dance"]
 '''
+# Only change from end of Task 2 should be creation of #students_approved variable with remaining students
 
 students = ["John", "Doe", "Jane", "Smith"]
 grades = [85, 90, 78, 88]
@@ -14,5 +15,5 @@ profile1 = [students[0], grades[0], activities[0]]
 profile2 = [students[1], grades[1], activities[1]]
 profile4 = [students[3], grades[3], activities[3]]
 
-remaining_students = (profile1, profile2, profile4)
-print(remaining_students)
+students_approved = (profile1, profile2, profile4)
+print(students_approved)
