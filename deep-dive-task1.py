2dc39e6ddec25898b15829e2c4a014d7a375b669
balkanarchist <balkanarchist@yandex.com>
2024-03-05 20:44:40 -0700

    added comment to deep-dive-task1

---
+++ deep-dive-task1.py
@@ -0,0 +1,14 @@
+'''
+You're organizing a school event, and you have lists containing student names,
+their grades, and the activities they're interested in.
+
+Task 1: Given the lists:
+
+students = ["John", "Doe", "Jane", "Smith"]
+grades = [85, 90, 78, 88]
+activities = ["Football", "Music", "Art", "Dance"]
+
+'''
+
+# Note there was no actual task provided with this assignment - we were
+# just given the information to be used on later tasks.
