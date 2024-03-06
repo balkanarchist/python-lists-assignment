afbe28d8c965fef96e79d7ea9682b1e4cc75a3f4
balkanarchist <balkanarchist@yandex.com>
2024-03-05 20:25:39 -0700

    added code to membership-operators-task3

---
+++ membership-operators-task3.py
@@ -0,0 +1,11 @@
+'''
+Given the list: numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Check if the number 7 is in the original numbers list.
+'''
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num = 7
+if num in numbers:
+    print("Yes, it's in the list.")
+else:
+    print("Nope. Go fish.")
